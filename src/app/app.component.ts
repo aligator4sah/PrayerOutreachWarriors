7aaabdb85e71382d8683de2a17459600535d9391
Aligator_sah <saixiongh@outlook.com>
2019-04-09 22:48:14 -0400

    Added ally package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+import { LyTheme2, ThemeVariables } from '@alyle/ui';
+
+const styles = (theme: ThemeVariables) => ({
+  '@global': {
+    body: { // Styles for `<body>` element
+      backgroundColor: theme.background.default,
+      color: theme.text.default,
+      fontFamily: theme.typography.fontFamily,
+      margin: 0,
+      direction: theme.direction
+    }
+  }
+});
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PrayerOutreachWarriors';
+  readonly classes = this.theme.addStyleSheet(styles);
+  constructor(
+    private theme: LyTheme2
+  ) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+/** Import animations */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/** Import Alyle UI */
+import {
+  LyThemeModule,
+  LY_THEME
+} from '@alyle/ui';
+
+/** Import the component modules */
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyToolbarModule } from '@alyle/ui/toolbar';
+import { LyResizingCroppingImageModule } from '@alyle/ui/resizing-cropping-images';
+
+/** Import themes */
+import { MinimaLight, MinimaDark } from '@alyle/ui/themes/minima';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Animations
+    BrowserAnimationsModule,
+    // Set main theme
+    LyThemeModule.setTheme('minima-light'),
+    // Add components
+    LyButtonModule,
+    LyToolbarModule,
+    LyResizingCroppingImageModule
   ],
-  providers: [],
+  providers: [
+    { provide: LY_THEME, useClass: MinimaLight, multi: true }, // name: `minima-light`
+    { provide: LY_THEME, useClass: MinimaDark, multi: true } // name: `minima-dark`
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
