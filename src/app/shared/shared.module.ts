0b44443ced3c5188b8b276dea50e76d2c44d4a8c
Aligator_sah <saixiongh@outlook.com>
2019-06-26 23:49:08 -0400

    Added pray list structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [],
@@ -14,14 +15,16 @@ import {MatListModule} from '@angular/material/list';
     MatCardModule,
     MatIconModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule
   ],
   exports: [
     MatCardModule,
     MatIconModule,
     HttpClientModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule
   ]
 })
 export class SharedModule { }
