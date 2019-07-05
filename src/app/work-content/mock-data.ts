76ca0d42f31b7887567cdda3ba0708ec76f56eba
Aligator_sah <saixiongh@outlook.com>
2019-07-04 23:52:12 -0400

    Added five prayer category

---
+++ src/app/work-content/mock-data.ts
@@ -0,0 +1,7 @@
+const CATEGORY = [
+    {name: "coworker or non-christion friends"},
+    {name: "family member or family situation"},
+    {name: "person in ministry"},
+    {name: "private prayer"},
+    {name: "global or large-scale event"}
+]
