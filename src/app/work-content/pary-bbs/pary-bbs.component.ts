180b7e5ba054889247030088fc1a81a67856a138
Aligator_sah <saixiongh@outlook.com>
2020-04-10 11:16:01 -0400

    Added cards for pray post

--- src/app/work-content/pary-bbs/pary-bbs.component.ts
+++ src/app/work-content/pary-bbs/pary-bbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pary-bbs',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParyBbsComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
+    iconRegistry.addSvgIcon(
+      'like',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/heart.svg'));
+  }
 
   ngOnInit() {
   }
